01d708d9751acad5c81ea916d9b913a6eb80f472
Marcus <marcusbullock@hotmail.co.uk>
2016-02-15 16:04:46 +0000

    Fizzbuzz nearly complete

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -7,7 +7,19 @@ describe ("fizzbuzz", function() {
   });
   describe('multiples of 3', function() {
     it('fizzes for 3', function() {
-      expect(fizzBuzz.play(6)).toEqual('fizz');
+      expect(fizzBuzz.play(3)).toEqual('fizz');
+    });
+  });
+
+  describe('multiples of 5', function() {
+    it('fizzes for 5', function() {
+      expect(fizzBuzz.play(5)).toEqual('buzz');
+    });
+  });
+
+  describe('mutliples of both 3 & 5', function() {
+    it('fizzes for 15', function() {
+      expect(fizzBuzz.play(15)).toEqual('fizzbuzz')
     });
   });
 
